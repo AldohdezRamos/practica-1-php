8f2592d346685e30334b0ba3e1ff799a900a7456
Aldo <aldo70551@gmail.com>
2024-06-19 08:06:06 -0600

    php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+echo "My first PHP script!";
+echo "<br>";
+echo "Mis datos:";
+$nombre= "Aldo Hernandez";
+$edad =18;
+$estatura = 1.69;
+echo "soy ..." .strtoupper($nombre). "            tengo" . $edad. "años y mido" .$estatura . "metros";
+echo "<br>";
+var_dump( is_int($nombre));
+echo "<br>";
+$x=34;
+$y=356.78;
+$z=$x+$y;
+echo "La suma es $x + $y = $z <br>";
+echo "La resta es $x -- $y = $z <br>";
+echo "La division es $x / $y = $z <br>";
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>practica php</title>
+</head>
+<body>
+    Hola
+</body>
+</html>
